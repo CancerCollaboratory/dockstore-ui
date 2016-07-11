cc48c260f3f58651792f91be3877e508240e7b0c
Janice Patricia <janice.patricia@oicr.on.ca>
2016-07-11 10:54:18 -0400

    moved update to inside return promise

--- app/scripts/controllers/workflowdetails.js
+++ app/scripts/controllers/workflowdetails.js
@@ -361,11 +361,11 @@ angular.module('dockstore.ui')
           $scope.setDefaultWorkflowPath($scope.workflowObj.id, $scope.workflowObj.workflow_path)
             .then(function(workflowObj) {
               $scope.labelsEditMode = false;
-            });
-          $scope.updateWorkflowPathVersion($scope.workflowObj.id, $scope.workflowObj.workflow_path)
-            .then(function(workflowObj) {
-              $scope.labelsEditMode = false;
-              $scope.refreshWorkflow($scope.workflowObj.id,0);
+              $scope.updateWorkflowPathVersion($scope.workflowObj.id, $scope.workflowObj.workflow_path)
+                .then(function(workflowObj) {
+                  $scope.labelsEditMode = false;
+                  $scope.refreshWorkflow($scope.workflowObj.id,0);
+                });
             });
         }
       };
