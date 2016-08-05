ebcb27726ad0e284d5de476f3b8fc12888137df5
Janice Patricia <janice.patricia@oicr.on.ca>
2016-08-04 21:48:25 -0400

    revert back to not ignoring sync

--- test/e2e/app-spec.js
+++ test/e2e/app-spec.js
@@ -6,7 +6,7 @@
 describe('dockstore homepage', function() {
 
 	beforeEach(function () {
-	  browser.ignoreSynchronization = true; //to avoid error angular is not found --> could be because of syncing problem
+	  //browser.ignoreSynchronization = true; //to avoid error angular is not found --> could be because of syncing problem
 		browser.get('index.html');
 	});
 
