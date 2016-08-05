72d3730d5bb0af7588ded660660e885fb71f8ab9
Denis Yuen <denis.yuen@oicr.on.ca>
2016-08-05 16:49:11 -0400

    Possible to skip Selenium with Chrome
    http://www.protractortest.org/#/server-setup#connecting-directly-to-browser-drivers

--- e2e.conf.js
+++ e2e.conf.js
@@ -2,18 +2,7 @@ exports.config = {
 
 	allScriptsTimeout: 99999,
 
-	// The address of a running selenium server.
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
-
-	// The location of the selenium standalone server .jar file, relative
-	// to the location of this config. If no other method of starting selenium
-	// is found, this will default to
-	// node_modules/protractor/selenium/selenium-server...
-	seleniumServerJar: 'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar',
-
-	// The port to start the selenium server on, or null if the server should
-	// find its own unused port.
-	seleniumPort: 4444,
+	directConnect: true,
 
 	// Capabilities to be passed to the webdriver instance.
 	capabilities: {
